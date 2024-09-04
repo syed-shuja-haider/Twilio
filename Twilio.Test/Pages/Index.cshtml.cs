43fe48953699f097402ec145fef993cd2b40ac4f
Syed Shuja Haider Sharazi <shuja.haider@markhortechnologies.com>
2024-09-04 20:29:03 +0500

    Iniital Commit

---
+++ Twilio.Test/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Twilio.Test.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        private readonly TwilioService _twilioService;
+
+        public IndexModel(ILogger<IndexModel> logger, TwilioService twilioService)
+        {
+            _logger = logger;
+            _twilioService = twilioService;
+        }
+
+        public void OnGet()
+        {
+            _twilioService.SendMessage("+923000477761", "Testing");
+        }
+    }
+}
---
+++ Twilio.Test/TwilioService.cs
@@ -0,0 +1,34 @@
+﻿using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace Twilio.Test
+{
+    public class TwilioService
+    {
+        private readonly string _accountSid;
+        private readonly string _authToken;
+        private readonly string _twilioPhoneNumber;
+
+        public TwilioService(IConfiguration configuration)
+        {
+            _accountSid = configuration["Twilio:AccountSID"];
+            _authToken = configuration["Twilio:AuthToken"];
+            _twilioPhoneNumber = configuration["Twilio:PhoneNumber"];
+        }
+
+        public void SendMessage(string toPhoneNumber, string message)
+        {
+            TwilioClient.Init(_accountSid, _authToken);
+
+            var messageOptions = new CreateMessageOptions(
+                new PhoneNumber(toPhoneNumber))
+            {
+                From = new PhoneNumber(_twilioPhoneNumber),
+                Body = message
+            };
+
+            var msg = MessageResource.Create(messageOptions);
+            Console.WriteLine($"Message sent: SID {msg.Sid}");
+        }
+    }
+}
